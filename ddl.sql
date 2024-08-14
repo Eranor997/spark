e387ad44b33fecedfb3041246194662da12c6bf7
Vladimir Markov <103460271+Eranor997@users.noreply.github.com>
2024-08-14 20:03:04 +0500

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,12 +1,8 @@
 CREATE TABLE default.shkDefect_verdict_local
 (
-
     `shk_id` Int64,
-
     `place_code` UInt32,
-
     `reason_id` UInt32,
-
     `employee_id` UInt32,
     `office_id` UInt32
 )

9e7c7554014dc91f332666a9f7fec288cfab4632
Vladimir Markov <103460271+Eranor997@users.noreply.github.com>
2024-08-14 19:36:49 +0500

    Add files via upload

---
+++ ddl.sql
@@ -0,0 +1,15 @@
+CREATE TABLE default.shkDefect_verdict_local
+(
+
+    `shk_id` Int64,
+
+    `place_code` UInt32,
+
+    `reason_id` UInt32,
+
+    `employee_id` UInt32,
+    `office_id` UInt32
+)
+ENGINE = MergeTree
+ORDER BY (shk_id)
+SETTINGS index_granularity = 8192;
